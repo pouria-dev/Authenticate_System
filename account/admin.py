381072e75d5627203400a6ae050f40ccd32d7c6d
Pouria Hajipour <pouria@dhcppc0>
2026-08-17 17:06:21 -0400

    Configure new user model model to admin panel

--- account/admin.py
+++ account/admin.py
@@ -6,8 +6,7 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
-
-from customauth.models import MyUser
+from .models import MyUser
 
 
 class UserCreationForm(forms.ModelForm):
